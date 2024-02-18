7877536151c616c3edc9c74bb4bb12e457944777
lolzazayou9 <152173077+lolzazayou9@users.noreply.github.com>
2024-02-18 18:58:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkCode() {
     var input = document.getElementById("codeInput").value;
     var messageElement = document.getElementById("message");
-    if (input === "ABC") {
+    if (input === "ABCD") {
         messageElement.innerHTML = "น้องฟอสเองจ้า";
     } if (input === "ควย"){
         messageElement.innerHTML = "ควย";
